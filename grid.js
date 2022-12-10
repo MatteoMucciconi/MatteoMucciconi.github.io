9f5a6a5344fff9bfe13234714608e348891f018e
MatteoMucciconi <31467187+MatteoMucciconi@users.noreply.github.com>
2022-12-10 14:39:46 +0000

    Create grid.js

---
+++ grid.js
@@ -0,0 +1,7 @@
+// Get a reference to the grid
+var grid = document.querySelector('#grid');
+
+// Add an event listener to handle clicks on the grid cells
+grid.addEventListener('click', handleClick);
+
+// Function to handle clicks on the
