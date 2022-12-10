e5903307e257e1cc0ceda90eb8c62cae90a6454b
MatteoMucciconi <31467187+MatteoMucciconi@users.noreply.github.com>
2022-12-10 14:14:09 +0000

    Update drawing-widget.js

--- drawing-widget.js
+++ drawing-widget.js
@@ -1,45 +1,57 @@
-// Get a reference to the canvas element
-var canvas = document.querySelector('#drawing-canvas');
+const canvas = document.getElementById('drawing-board');
+const toolbar = document.getElementById('toolbar');
+const ctx = canvas.getContext('2d');
 
-// Get the 2D drawing context of the canvas
-var context = canvas.getContext('2d');
+const canvasOffsetX = canvas.offsetLeft;
+const canvasOffsetY = canvas.offsetTop;
 
-// Set the line width and color for the drawing
-context.lineWidth = 5;
-context.strokeStyle = '#333';
+canvas.width = window.innerWidth - canvasOffsetX;
+canvas.height = window.innerHeight - canvasOffsetY;
 
-// Variables to keep track of the current drawing state
-var isDrawing = false;
-var lastX = 0;
-var lastY = 0;
+let isPainting = false;
+let lineWidth = 5;
+let startX;
+let startY;
 
-// Add event listeners to handle the drawing
-canvas.addEventListener('mousedown', startDrawing);
+toolbar.addEventListener('click', e => {
+    if (e.target.id === 'clear') {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+});
+
+toolbar.addEventListener('change', e => {
+    if(e.target.id === 'stroke') {
+        ctx.strokeStyle = e.target.value;
+    }
+
+    if(e.target.id === 'lineWidth') {
+        lineWidth = e.target.value;
+    }
+    
+});
+
+const draw = (e) => {
+    if(!isPainting) {
+        return;
+    }
+
+    ctx.lineWidth = lineWidth;
+    ctx.lineCap = 'round';
+
+    ctx.lineTo(e.clientX - canvasOffsetX, e.clientY);
+    ctx.stroke();
+}
+
+canvas.addEventListener('mousedown', (e) => {
+    isPainting = true;
+    startX = e.clientX;
+    startY = e.clientY;
+});
+
+canvas.addEventListener('mouseup', e => {
+    isPainting = false;
+    ctx.stroke();
+    ctx.beginPath();
+});
+
 canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mouseup', stopDrawing);
-canvas.addEventListener('mouseout', stopDrawing);
-
-// Function to start drawing when the mouse is pressed
-function startDrawing(event) {
-    isDrawing = true;
-    lastX = event.offsetX;
-    lastY = event.offsetY;
-}
-
-// Function to draw a line segment when the mouse is moved
-function draw(event) {
-    if (!isDrawing) return;
-
-    context.beginPath();
-    context.moveTo(lastX, lastY);
-    context.lineTo(event.offsetX, event.offsetY);
-    context.stroke();
-
-    lastX = event.offsetX;
-    lastY = event.offsetY;
-}
-
-// Function to stop drawing when the mouse is released or leaves the canvas
-function stopDrawing() {
-    isDrawing = false;
-}
